8916be02c6771956a3350a8f8318c71641ad2e26
Steve Leonard <batman.xx@gmail.com>
2013-01-21 01:42:08 -0500

    move to single file

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ from setuptools import setup, find_packages
 install_requires = ['requests>=1.1.0']
 
 def test():
-    pep8 = 'pep8 pystmark tests'
-    pyflakes = 'pyflakes pystmark tests'
+    pep8 = 'pep8 pystmark.py tests'
+    pyflakes = 'pyflakes pystmark.py tests'
     cmds = [pep8, pyflakes]
     try:
         map(subprocess.check_call, map(shlex.split, cmds))
