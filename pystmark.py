886644edd25f2381fb3ab71fe11086b1cf5a8970
Steve Leonard <batman.xx@gmail.com>
2013-06-01 11:31:04 -0400

    bump version

--- pystmark.py
+++ pystmark.py
@@ -44,7 +44,7 @@ except ImportError:             # pragma: no cover
 
 
 __title__ = 'pystmark'
-__version__ = '0.2'
+__version__ = '0.3'
 __license__ = 'MIT'
 
 # Constant defined in the Postmark docs:
